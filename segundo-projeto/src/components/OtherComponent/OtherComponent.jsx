b7ee34b1dd6308cc1fe79ba89afebc0da5a639c1
Simycity <vhzfilho@hotmail.com>
2025-09-19 11:07:03 -0300

    Atualização de Pacote do segundo-projeto

---
+++ segundo-projeto/src/components/OtherComponent/OtherComponent.jsx
@@ -0,0 +1,15 @@
+import styles from "./OtherComponent.module.css"
+
+export const OtherComponent = () => {
+  return (
+    <div>
+        <h1 className={styles.titulo}>
+            Bara bara bara
+        </h1>
+        <h3 className={styles.bere}>
+            Bere bere bere
+        </h3>
+
+    </div>
+  )
+}
